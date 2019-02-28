5e2846ca000862600f000b856744677cb0e34bef
Lereanne Mendoza <lcmendoz@ucsd.edu>
2019-02-28 10:39:18 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
-}
+	L.mapquest.key = 'eAzQ2BhYkpFdrdfEW3rv6ZZzX754cGYu';
+
+	var map = L.mapquest.map('map', {
+		center: [32.878803,  -117.235912], 
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12, 
+		zoomControl: false
+	});
+
+	// Add a marker to the map
+	L.marker([32.878803, -117.235912]).addTo(map);
+} 
